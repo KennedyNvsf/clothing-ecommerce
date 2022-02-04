7ac37e86d68466557be7d681f893174eeaa29fed
Webstack-Corporation <webstackcorp@gmail.com>
2022-02-04 18:10:29 +0000

    Folder structure setup, finihsed menu-item styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import HomePage from "./pages/HomePage";
+import "./styles/app.styles.scss";
+
+//pages
+import HomePage from "./pages/home-page/HomePage";
+//components
+
 
 
 function App() {
---
+++ src/pages/home-page/HomePage.jsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+import "../home-page/homepage.styles.scss";//styles
+//components
+import Directory from "../../components/directory/Directory.component";
+
+
+const HomePage = () => {
+
+    return (
+
+        <div className="homePage">
+            <Directory/>
+        </div>
+    )
+}
+
+export default HomePage;
